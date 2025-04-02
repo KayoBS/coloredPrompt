c7ae9c911a0a59430cf7f2200223c2437a34af7b
KayoBS <kayobs@hotmail.com>
2025-04-02 12:21:50 -0300

    Começo da criação da função printc()

---
+++ printc.hpp
@@ -0,0 +1,9 @@
+#ifndef PRINT_COLORED_HPP_INCLUDED
+#define PRINT_COLORED_HPP_INCLUDED
+
+void printc( const char* wordColor );
+void printc( const char* wordColor, const char* backgroundColor );
+void printc( const char* wordColor, const char* backgroundColor, bool highIntensity );
+void printc( const char* wordColor, const char* backgroundColor, bool highIntensity, bool bold );
+
+#endif
